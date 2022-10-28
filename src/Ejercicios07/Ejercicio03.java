2e7ea43968bf642ec8a0a0eab7aa6454118a2bf3
hdr24 <114582077+hdr24@users.noreply.github.com>
2022-10-28 14:19:06 +0200

    Update Ejercicio03.java

--- src/Ejercicios07/Ejercicio03.java
+++ src/Ejercicios07/Ejercicio03.java
@@ -2,18 +2,17 @@ package Ejercicios07;
 
 import java.util.Scanner;
 
-public class Ejercicio3 {
+public class Ejercicio03 {
     public static void main(String[] args) {
-        int num;
-        int[] numReves = new int[10];
+        int[] num = new int[10];
         Scanner s = new Scanner(System.in);
         System.out.println("Introduzca 10 números: ");
 
         for (int i = 0; i < 10; i++) {
-            numReves[i] = s.nextInt();
+            num[i] = s.nextInt();
         }
         for (int i = 9; i >= 0; i--) {
-            System.out.print(numReves[i] + " ");
+            System.out.print(num[i] + " ");
         }
     }
 }
