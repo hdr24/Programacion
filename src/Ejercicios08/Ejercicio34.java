729cf974ff90bbf1ddba9fe28fc09305782509a1
hdr24 <114582077+hdr24@users.noreply.github.com>
2022-12-13 13:01:19 +0100

    Update Ejercicio34.java

--- src/Ejercicios08/Ejercicio34.java
+++ src/Ejercicios08/Ejercicio34.java
@@ -1,9 +1,15 @@
 package Ejercicios08;
 
 public class Ejercicio34 {
-    public static int[] diagonal (int[][] array, int coorY, int coorX, String dir) {
-        if (dir.equals("nose")) {
-            while ()
+    public static int[] diagonal (int[][] array, int fila, int col, String dir) {
+        // Comprobamos que el string pedido es correcto o si la fila o columna estan out of bounds
+        if (!(dir.equals("nose") || dir.equals("neso")) || fila > array.length || col > array.length) {
+            return new int[]{-1};
         }
+        return new int[]{-2};
+    }
+
+    public static void main(String[] args) {
+
     }
 }
